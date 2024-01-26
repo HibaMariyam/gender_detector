5b1df4254043ce817b9c9b43bb3a17b73e38afe2
HibaMariyam <hibunalakath@gmail.com>
2024-01-26 13:58:53 +0530

    gender_detector_ui

--- lib/home/views/home_page.dart
+++ lib/home/views/home_page.dart
@@ -14,74 +14,127 @@ class _HomePageState extends State<HomePage> {
   String userGender='';
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        
+      ),
       body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-                    children: [
-            Text("do you what to know your gender?"),
-            SizedBox(height: 20),
-            TextField(
-              controller: _genderController,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: 'Enter your name'),
-             
-            ),
-            Text('Click on the button'),
-            SizedBox(height: 20),
-            ElevatedButton(
-                onPressed: () async {
-                  context
-                      .read<GenderCubit>()
-                      .genderDetector(_genderController.text);
-                      
-                },
-                child: Text('Know your gender')),
-            SizedBox(
-              height: 20,
-            ),
-            BlocConsumer<GenderCubit, GenderState>(
-              listener: (context, state) {
-               if(state is GenderFailure){
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error')));
-               }
-               if(state is GenderSuccess){
-            
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Gender detected')));
-                setState(() {
-                  
-                  userGender=state.gender.gender ?? "Other";
-                  
-                }
-                  
-                
-                );
-                
-               }
-            
-              },
-              builder: (context,state) {
-                if(state is GenderFailure){
-                  return Text(state.msg);
-                }
-                
-                // state is GenderLoading
-                // ?CircularProgressIndicator()
-      
-                //:Text(userGender??'');
-                 if(state is GenderLoading){
-                  return CircularProgressIndicator();
-                }
-                 return Text(userGender??'');
-              }
-             
+           child: Padding(
+             padding: const EdgeInsets.symmetric(
+              horizontal: 20
+             ),
+             child: Column(
+                     children: [
+        
+             Text("Do you what to know your gender?",
+             style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+               fontSize: 30,
+               color: Colors.black,
+               fontWeight: FontWeight.bold,
                
+             ),
+             ),
+             SizedBox(height: 30),
+             Container(
+           
+              decoration: BoxDecoration(
+                gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 244, 202, 231),
+                  Color.fromARGB(255, 154, 202, 242)
+                ])
+              ),
+               child: TextField(
+                 controller: _genderController,
+                 decoration: InputDecoration(
+                     border: OutlineInputBorder(), hintText: 'Enter your name'),
                 
+               ),
+             ),
+             SizedBox(height: 20),
+             ElevatedButton(
+                 
+                 onPressed: () async {
+                   context
+                       .read<GenderCubit>()
+                       .genderDetector(_genderController.text);
+                       
+                 },
+                 style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 45),
+                  backgroundColor: const Color.fromARGB(169, 255, 193, 7)
+                 ),
+                 
+                 child: Text('Click here',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+    
+               fontWeight: FontWeight.w500,
+               
+             ),
+                 )
+                 ),
+             SizedBox(
+               height: 20,
+             ),
+             BlocConsumer<GenderCubit, GenderState>(
+               listener: (context, state) {
+                if(state is GenderFailure){
+                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Error')));
+                }
+                if(state is GenderSuccess){
+             
+                 ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Gender detected')));
+                 setState(() {
+                   
+                   userGender=state.gender.gender?? "Other";
+                   
+                 }
+                 );
+                 
+                }
+             
+               },
+               builder: (context,state) {
+                 if(state is GenderFailure){
+                   return Text(state.msg);
+                 }
+               
+                  if(state is GenderLoading){
+                   return CircularProgressIndicator();
+                 }
+                  return Text(userGender??'Other',
+                  
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  )
+                  );
+               }
               
-            )
-                    ],
-                  ),
-          )),
-    );
+                
+                 
+               
+             ),
+             SizedBox(height: 15,),
+              if(userGender == 'male')
+      Image.asset('assets/images/male1.png',
+      height: 50,
+      width: 50,
+      ),
+    if (userGender == 'female')
+     Image.asset('assets/images/female1.png',
+     height: 50,
+      width: 50,
+     ),
+
+                     ],
+                   ),
+           ),
+         ),
+      );
+    
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gender_detector/home/cubit/gender_cubit.dart';
 import 'package:gender_detector/home/views/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,16 +11,18 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => GenderCubit(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
           title: 'Gender Detector',
           theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
+            textTheme: GoogleFonts.latoTextTheme(),
+            colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 179, 111, 157)),
+            
+        useMaterial3: true,
           ),
           home: HomePage()),
     );
